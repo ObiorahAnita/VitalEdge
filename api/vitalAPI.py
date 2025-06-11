ed8357a8a0f9a7d3c210759d179d96ab498806b5
ObiorahAnita <obiorahanita1@gmail.com>
2025-06-11 18:12:19 +0200

    API and database has been updated

--- api/vitalAPI.py
+++ api/vitalAPI.py
@@ -29,6 +29,21 @@ class RecordsData(BaseModel):
     lat: Optional[float] = None
     lon: Optional[float] = None
     emerg: Optional[int] = None
+
+class PatientData(BaseModel):
+    """
+    Defines the structure of the record table 
+    """
+    device_id: str
+    record_date: str
+    steps: Optional[int] = None 
+    heartrate: Optional[float] = None  
+    oxygen: Optional[float] = None  
+    humidity: Optional[float] = None
+    temp: Optional[float] = None
+    co2: Optional[int] = None
+    records_inserted: Optional[int] = None
+    emerg: Optional[int] = None    
     
 
 class UserLocation(BaseModel):
@@ -81,7 +96,6 @@ async def records(
     db: sqlite3.Connection = Depends(get_db)
 ):
     cursor = db.cursor()
-    #cursor.execute(device_id, record_time, steps, heartrate, oxygen) - wrong approach
     cursor.execute("SELECT * FROM records")
     data = cursor.fetchall()
     return[
@@ -90,18 +104,10 @@ async def records(
             record_date=row['record_date'],
             steps=row['steps'],
             heartrate=row['heartrate'],
-            avg_heartrate=row['avg_heartrate'],
-            peak_heartrate=row['peak_heartrate'],
             oxygen=row['oxygen'],
-            avg_oxygen=row['avg_oxygen'],
             humidity=row['humidity'],
-            avg_humidity=row['avg_humidity'],
             temp=row['temp'],
-            avg_temp=row['avg_temp'],
             co2=row['co2'],
-            avg_co2=row['avg_co2'],
-            lat=row['lat'],
-            lon=row['lon'],
             emerg=row['emerg']
         ) for row in data
     ]
@@ -111,34 +117,55 @@ async def retrieve_user_location (location: UserLocation):
     with sqlite3.connect(DB_PATH) as db:
         # print(location.device_id, location.record_date, location.lat, location.lon)
         cur = db.cursor()
-        cur.execute("INSERT INTO records (device_id, record_date, lat, lon) VALUES (?, ?, ?, ?)", (location.device_id, location.record_date, location.lat, location.lon))
+        cur = db.cursor()
+        cur.execute("""INSERT INTO records (device_id, record_date, lat, lon) VALUES (?, ?, ?, ?)"
+                    ON CONFLICT(device_id, record_date) DO UPDATE SET
+                    lat = excluded.lat,
+                    lon = excluded.lon
+        """, 
+        (location.device_id, location.record_date, location.lat, location.lon))
         db.commit()
     print(f"Data inserted: device_id{location.device_id}, record_date{location.record_date}, lat{location.lat}, lon{location.lon}")
     return{"status": "successful",
            "data": location.model_dump()}
 
-
+    
 @app.post('/user_data')
-async def retrieve_user_data(data: RecordsData):
+async def retrieve_user_data(data: PatientData):
     with sqlite3.connect(DB_PATH) as db:
         # print(data.device_id, data.record_date, data.steps, data.heartrate, data.avg_heartrate, data.peak_heartrate, data.oxygen, 
         #       data.avg_oxygen, data.humidity, data.avg_humidity, data.temp, data.avg_temp, data.co2, data.avg_co2, data.emerg)
         cur = db.cursor()
-        # cur.execute("INSERT INTO records (device_id, record_date, steps, heartrate, avg_heartrate, peak_heartrate, oxygen, avg_oxygen, humidity, avg_humidity, temp, avg_temp, co2, avg_co2, emerg) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-        #             data.device_id, data.record_date, data.steps, data.heartrate, data.avg_heartrate, data.peak_heartrate, data.oxygen, 
-        #             data.avg_oxygen, data.humidity, data.avg_humidity, data.temp, data.avg_temp, data.co2, data.avg_co2, data.emerg)
         cur.execute(
             """
             INSERT INTO records (
                 device_id, record_date, steps, heartrate, avg_heartrate, peak_heartrate,
                 oxygen, avg_oxygen, humidity, avg_humidity, temp, avg_temp,
-                co2, avg_co2, lat, lon, emerg
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                co2, avg_co2, records_inserted, emerg
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT(device_id, record_date) DO UPDATE SET
+            steps = excluded.steps,
+            heartrate = excluded.heartrate,
+            oxygen = excluded.oxygen,
+            humidity = excluded.humidity,
+            temp = excluded.temp,
+            co2 = excluded.co2,
+            emerg = excluded.emerg,
+            avg_heartrate = ((records.avg_heartrate * records.records_inserted) + excluded.heartrate) / (records.records_inserted + 1),
+            avg_oxygen    = ((records.avg_oxygen * records.records_inserted) + excluded.oxygen) / (records.records_inserted + 1),
+            avg_humidity  = ((records.avg_humidity * records.records_inserted) + excluded.humidity) / (records.records_inserted + 1),
+            avg_temp      = ((records.avg_temp * records.records_inserted) + excluded.temp) / (records.records_inserted + 1),
+            avg_co2       = ((records.avg_co2 * records.records_inserted) + excluded.co2) / (records.records_inserted + 1),
+            peak_heartrate = 
+            CASE 
+                WHEN excluded.heartrate > records.peak_heartrate THEN excluded.heartrate
+                ELSE records.peak_heartrate
+            END,
+            records_inserted = records.records_inserted + 1
             """,
             (
-                data.device_id, data.record_date, data.steps, data.heartrate, data.avg_heartrate,
-                data.peak_heartrate, data.oxygen, data.avg_oxygen, data.humidity, data.avg_humidity,
-                data.temp, data.avg_temp, data.co2, data.avg_co2, data.lat, data.lon, data.emerg
+                data.device_id, data.record_date, data.steps, data.heartrate, data.heartrate, data.heartrate, data.oxygen, data.oxygen, data.humidity, data.humidity,
+                data.temp, data.temp, data.co2, data.co2, data.records_inserted, data.emerg
             )
         )
         db.commit()
@@ -147,5 +174,7 @@ async def retrieve_user_data(data: RecordsData):
         # print(f"\n humidity{data.humidity}, avg_humidity{data.avg_humidity}, temp{data.temp}, avg_temp{data.avg_temp}")
         # print(f"\n co2{data.co2}, avg_co2{data.avg_co2}, emerg{data.emerg}")
         return{"status": "successful",
-               "data": data.model_dump()}
+               "data": data.model_dump()}    
 #uvicorn vitalAPI:app --reload
+
+#uvicorn vitalAPI:app --host 0.0.0.0 --port 8000
